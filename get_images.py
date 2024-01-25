a5b76f497ae57af20496a975beb2de074871f786
Manas <manaslegodesigns@gmail.com>
2024-01-25 09:21:17 +0800

    adds images

--- get_images.py
+++ get_images.py
@@ -1,14 +1,27 @@
 import os
 import requests
+import random
 
 # Base directory for dataset
 base_dir = 'Yoga-82/Dataset/'
 
+count = 0
 # Loop through each file in the directory containing links
 for file in os.listdir('Yoga-82/yoga_dataset_links'):
+    #print all files in the directory
+    print(file)
+    # count length
+    count += 1
     if file.endswith(".txt"):
         with open(os.path.join('Yoga-82/yoga_dataset_links', file)) as f:
-            for line in f:
+            # Read all lines into a list
+            lines = f.readlines()
+            # Determine the smaller number between 100 and the total number of lines
+            num_lines_to_download = min(100, len(lines))
+            # Randomly select lines
+            selected_lines = random.sample(lines, num_lines_to_download)
+
+            for line in selected_lines:
                 try:
                     # Split line into name and URL
                     name, url = line.strip().split('\t')
@@ -32,3 +45,5 @@ for file in os.listdir('Yoga-82/yoga_dataset_links'):
                     print(f"Downloaded {name}")
                 except Exception as e:
                     print(f"Error processing {line}: {e}")
+
+print(f"Processed {count} files")
